dfcca1e5d24e905f5e8a9f793f876c44466ee428
Rachel Damasceno <rachelemilyADS@gmail.com>
2024-11-11 15:52:02 -0300

    tela e botoes

--- index.js
+++ index.js
@@ -1,57 +1,75 @@
-let iniciar = document.querySelector("#iniciar");
-let parar = document.querySelector("#parar");
-let zerar = document.querySelector("#zerar");
-let tela = document.querySelector("#tela");
+document.addEventListener("DOMContentLoaded", () => {
+  const buttons = document.querySelectorAll(".tempo-menu button");
+  const iniciar = document.querySelector("#iniciar");
+  const parar = document.querySelector("#parar");
+  const zerar = document.querySelector("#zerar");
+  const tela = document.querySelector("#tela");
 
-let min = 0;
-let sec = 0;
-let hora = 0;
-let ok = false;
+  let min = 0;
+  let sec = 0;
+  let hora = 0;
+  let ok = false;
+  let contador;
+  var noSleep = new NoSleep();
 
-iniciar.addEventListener("click", function (event) {
-  if (!ok) {
-    ok = true;
-    // A cada 1000 milisegundo a função Iniciar é chamada
-    contador = setInterval(Iniciar, 1000);
+  // Função para marcar o botão como selecionado
+  function selecionarBotao(button) {
+    buttons.forEach((b) => b.classList.remove("selected"));
+    button.classList.add("selected");
   }
-});
 
-zerar.addEventListener("click", function (event) {
-  console.log("Contador zerou");
-  clearInterval(contador);
-  min = 0;
-  sec = 0;
-  hora = 0;
-  ok = false;
-  tela.innerHTML = `${hora < 10 ? "0" + hora : hora}:${
-    min < 10 ? "0" + min : min
-  }:${sec < 10 ? "0" + sec : sec}`;
-  console.log(ok);
-});
-
-parar.addEventListener("click", function (event) {
-  clearInterval(contador);
-  console.log("Contador parou");
-
-  ok = false;
-  console.log(ok);
-});
-
-function Iniciar() {
-  if (ok) {
-    sec++;
-
-    if (sec === 60) {
-      sec = 0;
-      min++;
+  iniciar.addEventListener("click", function () {
+    if (!ok) {
+      ok = true;
+      contador = setInterval(Iniciar, 1000);
+      selecionarBotao(iniciar);
     }
-    if (min === 60) {
-      min = 0;
-      hora++;
-    }
-    // Adicionar 0 na frente do numero caso ele seja menor que 10, se não fica normal.
+    document.addEventListener(
+      "click",
+      function enableNoSleep() {
+        document.removeEventListener("click", enableNoSleep, false);
+        noSleep.enable();
+      },
+      false
+    );
+  });
+
+  parar.addEventListener("click", function () {
+    clearInterval(contador);
+    ok = false;
+    selecionarBotao(parar);
+    noSleep.disable();
+  });
+
+  zerar.addEventListener("click", function () {
+    clearInterval(contador);
+    min = 0;
+    sec = 0;
+    hora = 0;
+    ok = false;
     tela.innerHTML = `${hora < 10 ? "0" + hora : hora}:${
       min < 10 ? "0" + min : min
     }:${sec < 10 ? "0" + sec : sec}`;
+    selecionarBotao(zerar);
+    noSleep.disable();
+  });
+
+  function Iniciar() {
+    if (ok) {
+      sec++;
+
+      if (sec === 60) {
+        sec = 0;
+        min++;
+      }
+      if (min === 60) {
+        min = 0;
+        hora++;
+      }
+
+      tela.innerHTML = `${hora < 10 ? "0" + hora : hora}:${
+        min < 10 ? "0" + min : min
+      }:${sec < 10 ? "0" + sec : sec}`;
+    }
   }
-}
+});
